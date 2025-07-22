56dbbdc1b969ca8064536a1f2f5692628726e043
Ian Low <ianlow27@gmail.com>
2025-07-22 16:32:27 +0100

    0.0.2_250722-1731 - 250722 - initial basic functionality

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# Changelog 
+# Changelog
 
 ## TO DO LIST
 - : to do item 1
@@ -6,6 +6,9 @@
 - : to do item 3
 
 ## NOTES
+
+## 0.0.2_250722-1731 - 250722 - initial basic functionality
+-  Amendments as per the release description
 
 ## 0.0.1_250722-1248 - Initial version
 - a: initial comment
--- shpfyprods.php
+++ shpfyprods.php
@@ -1,20 +1,77 @@
 <?php
 $usage = "
-  Usage: php $argv[0] [-h]
-Version: 0.0.1_250722-1248
+  Usage: php $argv[0] [-h] <location-of-JSON-file>
+Version: 0.0.2_250722-1731
   About: $argv[0] Helps querying of Shopify products JSON file
  Author: Ian Low | Date: 2025-07-22 | Copyright (c) 2025 Ian Low | License: MIT
 Options:
     -h   Display help information including run options
     -n   Create a new instance
 ";
+
 if(isset($argv[1])){
   if($argv[1]=="-h"){
     echo $usage;
+    exit();
   }else if($argv[1]=="-n"){  
     echo "Please enter the following information or press 'Enter' for default...\n";
     echo "Project name (defaults to 'myprojphp'): "; $projname = trim(readline());
     if($projname=="") $projname = "myprojphp";
+    exit();
   }
 }
+
+
+if(!isset($argv[1])){
+  echo $usage;
+  exit();
+}
+
+$currprod="";
+$currhandle="";
+$currsibvar="";
+$curropt1="";
+
+$prevprod="";
+$count=0;
+foreach(explode("\n", file_get_contents( $argv[1] ) 
+    . "\n      \"title\": \"EndofFile\",") as $line){
+
+  //echo $line. "\n";
+  if(preg_match("/^\s\s\s\s\s\s\"title\":/", $line)){
+    echo $count."|".$currprod."|"
+      .$currhandle."|"
+      .$currsibvar."|"
+      .$curropt1."|"
+      ."\n";
+    $prevprod = $currprod;
+    //-----------------------------------------
+    $currprod=substr($line,16,-2);
+    $currhandle="";
+    $currsibvar="";
+    $curropt1="";
+    $count++;
+  }else if(preg_match("/^\s\s\s\s\s\s\"handle\":/", $line)){
+    $currhandle=substr($line,17,-2);
+  }else if(preg_match("/^\s\s\s\s\s\s\s\s\"variant_/", $line)){
+    $currsibvar=preg_replace("/[\"\,]/", "", substr($line,9));
+  }else if( (preg_match("/^\s\s\s\s\s\s\s\s\s\s\"option1\": \"/", $line)) && ($curropt1=="") ){
+    $curropt1=preg_replace("/[\"\,]/", "", substr($line,21));
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
